052b374459ddce8a225eecf2161ba67378ee5ba1
Shobhit Tomar <shobhit210@outlook.com>
2018-10-05 23:49:02 +0530

    initial2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { AuthComponent } from './auth/auth.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AuthComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
