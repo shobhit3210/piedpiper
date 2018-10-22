868a94bba1badeaeb3ea932585193c81e4dc298d
Shobhit Tomar <shobhit210@outlook.com>
2018-10-22 17:52:51 +0530

    changes in registration form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 const appRoutes: Routes = [
     { path: '', component: LoginComponent},
-    { path: 'login', component: LoginComponent},
+    { path: 'login', component: LoginComponent, children: [
+        { path: 'dashboard', component: UserDashboardComponent}
+    ]
+    },
     { path: 'register', component: RegisterComponent}
   ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,28 @@ import { LoginComponent } from './auth/login/login.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './material-model';
 import { AppRoutingModule } from './app-routing.module';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
+import { LayoutModule } from '@angular/cdk/layout';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    UserDashboardComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
